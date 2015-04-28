8ba490d025972c7f2bd8d4308e2b3df2c6cd50d0
EldinSalkovic <eldinsalkoviic@gmail.com>
2015-04-28 15:01:12 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,23 @@
+<?php
+	$adresa[0]="102 Evergreen Dr Springfield,TN 37172"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[0]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[0]="USA";
+	$telephone[0]=8008922847;// Broj koji se poziva na dugme CALL US
+
+	$adresa[1]=" 225 Roy Street,St-Eustache,QC J7R 5R5"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[1]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[1]="Canada";
+	$telephone[1]=8008922847;// Broj koji se poziva na dugme CALL US
+
+	$adresa[2]="102 Evergreen Dr Springfield,TN 37172"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[2]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[2]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[2]="Michael Prendergast";
+	$telephone[2]=8552284900;// Broj koji se poziva na dugme CALL US
+
+	$companynumber=3;
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="turn"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
